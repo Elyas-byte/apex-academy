4d68bac81f918104ed2f8d7c286384e3f76b2408
Elyas Rahimi <elyas.rahimi.4.28@gmail.com>
2025-05-08 20:15:05 +0300

    Removed brazillian

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     >
       <div className="text-center px-4">
         <h1 className="text-5xl md:text-6xl font-bold mb-4">Master the Art of </h1><h1 className="text-5xl md:text-6xl font-bold mb-4 text-blue-600">Jiu Jitsu</h1>
-        <p className="text-xl mb-8">Experience authentic Brazilian Jiu-Jitsu in Bahrain, brought to you by <br></br>Apex Academy Founded by real MMA fighters & black belts originally from the UK.</p>
+        <p className="text-xl mb-8">Experience authentic Jiu-Jitsu in Bahrain, brought to you by <br></br>Apex Academy Founded by real MMA fighters & black belts originally from the UK.</p>
         <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-3 px-6 rounded-lg transition" onClick={() => {
           const el = document.getElementById('register');
           el?.scrollIntoView({ behavior: 'smooth', block: 'start' });
